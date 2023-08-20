e65c419b67741dad159e6c015b4dacbb0ac80dca
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-08-20 18:14:39 -0500

    Navbar now has some CSS added, and it also has functions added to the navlinks to display and hide the menu based on the show menu status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/Navbar.css'
 
 function Navbar() {
+
+    const [menuStatus, setMenuStatus] = useState(false)
+
+    const showMenu = () => {
+        setMenuStatus(true)
+    }
+
+    const closeMenu = () => {
+        setMenuStatus(false)
+    }
+
   return (
     <div className='navbar'>
       <div className="logo__container">
-        <img src="/images/logo.png" alt="logo" />
+        <img className='logo' src="/images/logo.png" alt="logo" />
       </div>
 
-      <div className="navlinks__container">
-        <div className="navlink navlink1"><a href=''>Home</a></div>
-        <div className="navlink navlink2"><a href=''>About Us</a></div>
-        <div className="navlink navlink3"><a href=''>Prices</a></div>
-        <div className="navlink navlink4"><a href=''>Appointments</a></div>
-      </div>
+      {menuStatus ? (
+            <div className="navlinks__container">
+              <p onClick={closeMenu}>X</p>
+              <div className="navlink navlink1"><a href=''>Home</a></div>
+              <div className="navlink navlink2"><a href=''>About Us</a></div>
+              <div className="navlink navlink3"><a href=''>Prices</a></div>
+              <div className="navlink navlink4"><a href=''>Appointments</a></div>
+            </div>
+      ):(
+        <div className="burgerMenu" onClick={showMenu}>E</div>
+      )}
     </div>
   )
 }
