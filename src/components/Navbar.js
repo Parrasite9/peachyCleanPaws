376453e0f3182ab93fd3882e8984d9c13d341818
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-08-21 13:51:30 -0500

    CSS added so the navlinks are properly looking as they should. This is mobile only view.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,11 +39,13 @@ function Navbar() {
 
 
             <div className="navlinks__container">
+                <div className="navlinks__inside__container">
                 <div className="navlink navlink1"><a href=''>Home</a></div>
                 <div className="navlink navlink2"><a href=''>About Us</a></div>
                 <div className="navlink navlink3"><a href=''>Prices</a></div>
                 <div className="navlink navlink4"><a href=''>Appointments</a></div>
                 <div onClick={closeMenu}><CloseIcon /></div>
+                </div>
             </div>
 
             // DIFFERENCE BETWEEN COMMENTED OUT AND THE ABOVE CODE IS THE FIRST DIV. ONLY KEEPING THIS HERE FOR NOW
