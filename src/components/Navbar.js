ce8b5207b0bad6a24ed30b3b14a106e9b773a482
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-08-21 12:22:53 -0500

    MUI Icons added and navlinks have been put into a flex column.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import '../css/Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
 
@@ -33,14 +35,14 @@ function Navbar() {
 
       {menuStatus ? (
             <div className="navlinks__container">
-              <p onClick={closeMenu}>X</p>
               <div className="navlink navlink1"><a href=''>Home</a></div>
               <div className="navlink navlink2"><a href=''>About Us</a></div>
               <div className="navlink navlink3"><a href=''>Prices</a></div>
               <div className="navlink navlink4"><a href=''>Appointments</a></div>
+              <div onClick={closeMenu}><CloseIcon /></div>
             </div>
       ):(
-        <div className="burgerMenu" onClick={showMenu}>E</div>
+        <div className="burgerMenu" onClick={showMenu}><MenuIcon /></div>
       )}
     </div>
   )
