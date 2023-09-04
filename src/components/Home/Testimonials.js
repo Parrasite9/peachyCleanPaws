d45828fc49ba7f4f07b0da02d62eaca865d5018c
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-04 13:31:40 -0500

    Individual Slides has some altered data, dummy text, but no CSS at this time

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import '../../css/Home/Testimonials.css'
 
 export default class SimpleSlider extends Component {
   render() {
@@ -8,29 +9,38 @@ export default class SimpleSlider extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      pauseOnHover: true,
     };
     return (
-      <div>
+      <div className="testimonials">
         <h2> Single Item</h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">2</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
-          <div>
-            <h3>4</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">4</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
-          <div>
-            <h3>5</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">5</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
-          <div>
-            <h3>6</h3>
+          <div className="individual__Slide">
+            <h3 className="testimonial__Name">6</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure!</p>
           </div>
         </Slider>
       </div>
