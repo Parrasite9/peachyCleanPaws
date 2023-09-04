6e138d039e41a070bc0aae51d797667948efc165
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-04 12:35:50 -0500

    CSS has been added to Social Status

--- src/components/Home/SocialStatus.js
+++ src/components/Home/SocialStatus.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../../css/Home/SocialStatus.css'
 
 function SocialStatus() {
   return (
     <div className='socialStatus'>
       <h2>As Seen On</h2>
       <div className="Socials">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src="https://i.imgur.com/3mc6Pqp.png" alt="Entrepreneur" />
+        <img src="https://i.imgur.com/X1xpm7X.png" alt="Yahoo" />
+        <img src="https://i.imgur.com/Glrx8Ij.png" alt="Forbes" />
+        <img src="https://i.imgur.com/w31LVMZ.png" alt="NBC" />
       </div>
     </div>
   )
