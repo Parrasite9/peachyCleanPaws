02c225a407d8e2a7157bfd42b5f133912ba353da
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-04 12:13:40 -0500

    SocialStatus js and css has been added, basic data added into js and components have been altered (as far as the order they are displayed) within the Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,16 @@ import Navbar from '../Navbar'
 import Mission from './Mission'
 import Services from '../Home/Services/Services'
 import Footer from '../Footer'
+import SocialStatus from './SocialStatus'
 
 function Home() {
   return (
     <div className='home'>
       <Navbar />
       <Hero />
-      <Mission />
       <Services />
+      <SocialStatus />
+      <Mission />
       <Footer />
     </div>
   )
---
+++ src/components/Home/SocialStatus.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SocialStatus() {
+  return (
+    <div className='socialStatus'>
+      <h2>As Seen On</h2>
+      <div className="Socials">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default SocialStatus
