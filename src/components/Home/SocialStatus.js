f14a2cfd00f7dc4641b69b9c2c8bc9b8f6e4e6e9
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-04 12:55:29 -0500

    Further CSS has ben added for SocialStatus JS

--- src/components/Home/SocialStatus.js
+++ src/components/Home/SocialStatus.js
@@ -6,8 +6,8 @@ function SocialStatus() {
     <div className='socialStatus'>
       <h2>As Seen On</h2>
       <div className="Socials">
-        <img src="https://i.imgur.com/3mc6Pqp.png" alt="Entrepreneur" />
         <img src="https://i.imgur.com/X1xpm7X.png" alt="Yahoo" />
+        <img src="https://i.imgur.com/3mc6Pqp.png" alt="Entrepreneur" />
         <img src="https://i.imgur.com/Glrx8Ij.png" alt="Forbes" />
         <img src="https://i.imgur.com/w31LVMZ.png" alt="NBC" />
       </div>
