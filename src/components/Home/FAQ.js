6e31cfb28ebd840f2937aa24d0c8126ab39bb93e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-06 16:44:37 -0500

    FAQ accordian added and CSSd properly

--- src/components/Home/FAQ.js
+++ src/components/Home/FAQ.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PetsIcon from '@mui/icons-material/Pets';
+import '../../css/Home/FAQ.css'
 
-function FAQ() {
+export default function BasicAccordion() {
   return (
-    <div className='faq'>
-      
-    </div>
-  )
-}
+    <div className="faq__container">
+        <div className="faq__header">
+            <h3>Frequently Asked Questions</h3>
+        </div>
 
-export default FAQ
+        <div className='faq__subContainer'>
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<PetsIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                className='question'
+                >
+                <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+
+
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<PetsIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+                className='question'
+                >
+                <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+
+
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<PetsIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+                className='question'
+                >
+                <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+
+
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<PetsIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+                className='question'
+                >
+                <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+
+
+        </div>
+    </div>
+  );
+}
