789df5d25f956cc0f2352c299081da4e036778e1
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-06 13:40:01 -0500

    email functions has been created

--- src/components/Home/Email.js
+++ src/components/Home/Email.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Email() {
+
+    const [email, setEmail] = useState("")
+    const [isEmailValid, setIsEmailValid] = useState(true)
+
+    function handleInput(e) {
+        setEmail(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        if (email === "" || !/\S+@\S+\.\S+/.test(email)) {
+            setEmail(false)
+        } else {
+            setIsEmailValid(true);
+            alert(`Thank you for subscribing with ${email}`)
+            setEmail("")
+        }
+    }
+
   return (
     <div className='email'>
       THIS IS EMAIL
