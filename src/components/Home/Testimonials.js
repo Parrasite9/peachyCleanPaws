f0d7189792da768b4fdbdeecf0ca6b97cc655f90
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-09-06 12:40:18 -0500

    CSS and JS modifications has been made for Testimonials

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -5,7 +5,7 @@ import '../../css/Home/Testimonials.css'
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
